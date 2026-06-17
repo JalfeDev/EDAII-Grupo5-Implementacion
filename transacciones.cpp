9fb38439be9e413d4e3b533e2fe88048c39c57af
JalfeDev <juangustavo.loyolaf@gmail.com>
2026-06-17 17:22:55 -0500

    agregue el arbol red-black

--- transacciones.cpp
+++ transacciones.cpp
@@ -325,6 +325,156 @@ public:
 };
 
 
+enum Color { ROJO, NEGRO };
+
+class ColorNode {
+public:
+    int dato;
+    Color color;
+    ColorNode *izquierda, *derecha, *padre;
+
+    ColorNode(int valor) {
+        dato = valor;
+        color = ROJO;  // siempre se inserta rojo
+        izquierda = derecha = padre = nullptr;
+    }
+};
+
+class RedBlackTree {
+private:
+    ColorNode* raiz;
+
+    void rotarIzquierda(ColorNode*& raiz, ColorNode*& x) {
+        ColorNode* y = x->derecha;
+        x->derecha = y->izquierda;
+        if (y->izquierda != nullptr)
+            y->izquierda->padre = x;
+
+        y->padre = x->padre;
+
+        if (x->padre == nullptr)
+            raiz = y;
+        else if (x == x->padre->izquierda)
+            x->padre->izquierda = y;
+        else
+            x->padre->derecha = y;
+
+        y->izquierda = x;
+        x->padre = y;
+    }
+
+    void rotarDerecha(ColorNode*& raiz, ColorNode*& y) {
+        ColorNode* x = y->izquierda;
+        y->izquierda = x->derecha;
+        if (x->derecha != nullptr)
+            x->derecha->padre = y;
+
+        x->padre = y->padre;
+
+        if (y->padre == nullptr)
+            raiz = x;
+        else if (y == y->padre->izquierda)
+            y->padre->izquierda = x;
+        else
+            y->padre->derecha = x;
+
+        x->derecha = y;
+        y->padre = x;
+    }
+
+    void fixInsercion(ColorNode*& raiz, ColorNode*& nodo) {
+        ColorNode* padre = nullptr;
+        ColorNode* abuelo = nullptr;
+
+        while (nodo != raiz && nodo->color == ROJO && nodo->padre->color == ROJO) {
+            padre = nodo->padre;
+            abuelo = padre->padre;
+
+            // Padre es hijo izquierdo del abuelo
+            if (padre == abuelo->izquierda) {
+                ColorNode* tio = abuelo->derecha;
+
+                // Caso 1: el tío es rojo
+                if (tio != nullptr && tio->color == ROJO) {
+                    abuelo->color = ROJO;
+                    padre->color = NEGRO;
+                    tio->color = NEGRO;
+                    nodo = abuelo;
+                } else {
+                    // Caso 2: el nodo es hijo derecho
+                    if (nodo == padre->derecha) {
+                        rotarIzquierda(raiz, padre);
+                        nodo = padre;
+                        padre = nodo->padre;
+                    }
+                    // Caso 3: el nodo es hijo izquierdo
+                    rotarDerecha(raiz, abuelo);
+                    swap(padre->color, abuelo->color);
+                    nodo = padre;
+                }
+            }
+            // Padre es hijo derecho del abuelo
+            else {
+                ColorNode* tio = abuelo->izquierda;
+
+                if (tio != nullptr && tio->color == ROJO) {
+                    abuelo->color = ROJO;
+                    padre->color = NEGRO;
+                    tio->color = NEGRO;
+                    nodo = abuelo;
+                } else {
+                    if (nodo == padre->izquierda) {
+                        rotarDerecha(raiz, padre);
+                        nodo = padre;
+                        padre = nodo->padre;
+                    }
+                    rotarIzquierda(raiz, abuelo);
+                    swap(padre->color, abuelo->color);
+                    nodo = padre;
+                }
+            }
+        }
+
+        raiz->color = NEGRO;
+    }
+
+    void insertarNodo(ColorNode*& raiz, ColorNode*& nodo) {
+        if (raiz == nullptr) {
+            raiz = nodo;
+        } else if (nodo->dato < raiz->dato) {
+            insertarNodo(raiz->izquierda, nodo);
+            raiz->izquierda->padre = raiz;
+        } else {
+            insertarNodo(raiz->derecha, nodo);
+            raiz->derecha->padre = raiz;
+        }
+    }
+
+    void inorden(ColorNode* nodo) {
+        if (nodo == nullptr) return;
+        inorden(nodo->izquierda);
+        cout << nodo->dato << (nodo->color == ROJO ? "R" : "N") << " ";
+        inorden(nodo->derecha);
+    }
+
+public:
+    RedBlackTree() {
+        raiz = nullptr;
+    }
+
+    void insertar(int valor) {
+        ColorNode* nuevo = new ColorNode(valor);
+        insertarNodo(raiz, nuevo);
+        fixInsercion(raiz, nuevo);
+    }
+
+    void mostrarInorden() {
+        inorden(raiz);
+        cout << endl;
+    }
+};
+
+
 void cargarDesdeCSV(const string& filename, HashTable& hash, AVLTree& avl) {
   ifstream archivo(filename);
   if (!archivo.is_open()) {
